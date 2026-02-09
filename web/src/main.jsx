58b0b38cf65d9f2fdd4aee4d1bb98be4a98774f9
Ricardo Jorge Gonçalves Macedo <ricardo@Mac.home>
2026-02-09 00:21:08 +0000

    debug: replace app with minimal reproduction to isolate blank page issue

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -6,8 +6,12 @@ import { SecurityProvider } from './context/SecurityContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SecurityProvider>
+    <div style={{ color: 'red', fontSize: '2rem', padding: '2rem' }}>
+      <h1>Debug: Hello World</h1>
+      <p>If you see this, React is mounting.</p>
+    </div>
+    {/* <SecurityProvider>
       <App />
-    </SecurityProvider>
+    </SecurityProvider> */}
   </StrictMode>,
 )
